7fdaf23145445a8477ab3e0c5e42f48f35d693d8
Eliana Gimenez <eliana.gimenez@unnaki.com.py>
2019-08-08 17:47:11 -0400

    seeds

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -1,9 +1,26 @@
 <?php
 
 use Faker\Generator as Faker;
+use Faker\Provider\Image;
+use App\Category;
+use App\Level;
+use App\Teacher;
+use App\Course;
 
 $factory->define(App\Course::class, function (Faker $faker) {
+    $name = $faker->sentence;
+    $status = $faker->randomElement([Course::PUBLISHED, Course::PENDING, Course::REJECTED]);
+
     return [
-        //
+        'teacher_id' => Teacher::all()->random()->id,
+        'category_id' => Category::all()->random()->id,
+        'level_id' => Level::all()->random()->id,
+        'name' => $name,
+        'slug' => str_slug($name, '-'),
+        'description' => $faker->paragraph,
+        'picture' => Image::image(storage_path() . '/app/public/courses', 600, 350, 'business', false),
+        'status' => $status,
+        'previous_approved' => $status !== Course::PUBLISHED ? false : true,
+        'previous_rejected' => $status === Course::REJECTED ? true : false,
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Course;
 
 $factory->define(App\Review::class, function (Faker $faker) {
     return [
-        //
+        'course_id' => Course::all()->random()->id,
+        'rating' => $faker->randomFloat(2, 1, 5)
     ];
 });

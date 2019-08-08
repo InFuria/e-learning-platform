1ea2ec209373428659507c74c1635856a8219e3c
Eliana Gimenez <eliana.gimenez@unnaki.com.py>
2019-08-08 11:49:09 -0400

    all migrations and models configurated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Role;
 
 class CreateUsersTable extends Migration
 {
@@ -13,13 +14,54 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        //Se referencia aca al rol para que funcione la fk que se agrega abajo
+        Schema::create('roles', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name')->comment('Nombre del rol de usuario');
+            $table->text('description');
+            $table->timestamps();
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('role_id')->default(Role::STUDENT);
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->string('picture')->nullable()->comment('Imagen de usuario en la plataforma');
+
+
+            //Cashier columns (para manejo de suscripciones)
+            $table->string('stripe_id')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four')->nullable();
+            $table->string('trial_ends_at')->nullable();
+
+
             $table->timestamps();
+        });
+
+        Schema::create('subscriptions', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->string('stripe_id');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('user_social_acounts', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('provider')->comment('Facebook, Twitter, etc');
+            $table->string('provider_uid')->comment('Id del usuario en el provider usado para registrarse');
         });
     }
 
@@ -31,5 +73,8 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('user_social_acounts');
     }
 }
